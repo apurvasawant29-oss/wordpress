1dca0b8dfdd98eda7f573191e10c8ed97986f5aa
Apurva Sawant <apurvasawant29@gmail.com>
2025-12-21 15:11:23 +0530

    shop page edited

--- wp-content/themes/book_theme/functions.php
+++ wp-content/themes/book_theme/functions.php
@@ -31,3 +31,32 @@ function theme_enqueue_scripts() {
     wp_enqueue_style('theme-style', get_template_directory_uri() . '/css/theme-styles.css', '', filemtime(get_stylesheet_directory() . '/css/theme-styles.css'));
 
  }
+ //register widget sidebar for woocommerce shop page filters
+ function bookmarket_register_shop_sidebar() {
+    register_sidebar(array(
+        'name'          => 'WooCommerce Shop Sidebar',
+        'id'            => 'shop-sidebar',
+        'description'   => 'Filters for WooCommerce shop page',
+        'before_widget' => '<div class="shop-widget">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h4 class="shop-widget-title">',
+        'after_title'   => '</h4>',
+    ));
+}
+add_action('widgets_init', 'bookmarket_register_shop_sidebar');
+
+//rearrage price and add to cart button on product archive page
+remove_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_price', 10 );
+remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10 );
+
+add_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_price', 10 );
+add_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 20 );
+
+add_action( 'woocommerce_after_shop_loop_item', 'bookmarket_wrapper_start', 5 );
+function bookmarket_wrapper_start() {
+    echo '<div class="price-cart-wrapper">';
+}
+add_action( 'woocommerce_after_shop_loop_item', 'bookmarket_wrapper_end', 25 );
+function bookmarket_wrapper_end() {
+    echo '</div>';
+}
