49973435f86e19d31d6dba367c1ee5411a7f0048
akshatagdhuri-oss <akshatagdhuri@gmail.com>
2025-12-21 16:45:51 +0530

    footer changes

--- wp-content/themes/book_theme/footer.php
+++ wp-content/themes/book_theme/footer.php
@@ -1,2 +1,30 @@
-</body>
-</html>
+ <footer class="footer">
+  <div class="footer-wrapper">
+
+    <!-- Logo -->
+    <div class="footer-brand">
+      <img src="https://i.pinimg.com/736x/4c/99/f4/4c99f4486ca6434f3747b08614c60a7c.jpg" alt="Bookish">
+      <span>Bookish</span>
+    </div>
+
+    <!-- Navigation -->
+    <ul class="footer-nav">
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Services</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+
+    <!-- Social -->
+    <div class="footer-social">
+      <a href="#">Facebook</a>
+      <a href="#">Twitter</a>
+      <a href="#">Instagram</a>
+    </div>
+
+  </div>
+
+  <div class="footer-copy">
+    © 2025 Bookish. All rights reserved.
+  </div>
+</footer>
--- wp-content/themes/book_theme/functions.php
+++ wp-content/themes/book_theme/functions.php
@@ -30,6 +30,7 @@ function theme_enqueue_scripts() {
   
     wp_enqueue_style('theme-style', get_template_directory_uri() . '/css/theme-styles.css', '', filemtime(get_stylesheet_directory() . '/css/theme-styles.css'));
     wp_enqueue_style('poststyle', get_template_directory_uri() . '/css/poststyle.css', '', filemtime(get_stylesheet_directory() . '/css/poststyle.css'));
+  wp_enqueue_style('footer-style', get_template_directory_uri() . '/css/footer.css', '', filemtime(get_stylesheet_directory() . '/css/footer.css'));
 
  }
  //register widget sidebar for woocommerce shop page filters
